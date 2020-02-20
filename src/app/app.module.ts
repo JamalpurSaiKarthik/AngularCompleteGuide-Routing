d5be45043e8c3521b52c826da5be1c65fadeda83
JamalpurSaiKarthik <kartheek.js@gmail.com>
2020-02-20 18:34:44 +0530

    FinalRoutingChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {Routes,RouterModule} from '@angular/router'
-
-const appRoutes :Routes = [
-      {path:'',component:HomeComponent},
-      {path:'users',component:UsersComponent},
-      {path:'servers',component:ServersComponent},
-];
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -28,14 +25,18 @@ const appRoutes :Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    FormsModule,    
-    RouterModule.forRoot(appRoutes)
+    FormsModule,        
+    
+    //RouterModule
+    //RouterModule.forRoot(appRoutes)
   ],
-  providers: [ServersService],
+  providers: [ServersService,AuthGuard,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
